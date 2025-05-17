994b8cbcd5221436bb6afa83661af82d595515f7
profawesome1000 <abdulrahman.almasri1000@gmail.com>
2025-05-17 12:30:35 -0400

    Update MasseyHacks.py

--- MasseyHacks.py
+++ MasseyHacks.py
@@ -5,7 +5,7 @@ import time
 
 font.intit()
 Title = rect(475, 100, 300, 100)
-beaufort = font.Font("BeaufortforLOL-Medium.ttf", 15)
+beaufort = font.Font("That Sounds Great.otf", 40)
 width,height=1250,650
 screen=display.set_mode((width,height))
 RED=(255,0,0)
@@ -18,8 +18,13 @@ WHITE=(255,255,255)
 myClock=time.Clock()
 running=True
 background = Rect(0,0,width,height)
+Title = Rect(325, 50, 600, 125)
+About = Rect(475, 200, 300, 100)
+Start = Rect(375, 325, 500, 100)
+settings = Rect(475, 450, 300, 100)
+Openinglist = [Title, About, Start, settings]
+openingtext = ["Debate.io", "About", "Start", "Settings"]
 draw.rect(screen,(115,3,16), background)
-
 def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
@@ -35,12 +40,23 @@ PMC=300
 prep=900
 
 while running:
+    mx,my=mouse.get_pos()
+    mb=mouse.get_pressed()
     for evt in event.get():
         if evt.type==QUIT:
             running=False
-                  
-    mx,my=mouse.get_pos()
-    mb=mouse.get_pressed()
+     for i in Openinglist:
+        draw.rect(screen, (225,3,16), i)
+        textt = titletext.render(openingtext[Openinglist.index(i)],True, BLACK)
+        text_rect = textt.get_rect(center = i.center)
+        screen.blit(textt,text_rect)
+        if i.collidepoint(mx,my):
+            if Openinglist.index(i) != 0:
+                draw.rect(screen, WHITE, i, 2)
+            else:
+                draw.rect(screen,BLACK,i,2)
+        else:
+            draw.rect(screen,BLACK,i,2)              
       
     myClock.tick(60)
     display.flip()
